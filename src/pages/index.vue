105436cea0620e3fccb63606c24c4cbdf1315f96
caoyu412 <275125992@qq.com>
2017-07-20 16:30:56 +0800

    fix: add index animation

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,7 +4,9 @@
         <el-col :span="3"><div class="grid-content"></div></el-col>
         <el-col :span="18">
           <div class="content">
-            首页
+            <div class="main">
+              首页
+            </div>
           </div>
         </el-col>
         <el-col :span="3"><div class="grid-content"></div></el-col>
@@ -28,7 +30,51 @@ export default {
 
 <style scoped>
   .content {
+    position: relative;
     width: 100%;
-    height: auto;
+    height: 934px;
+  }
+  
+  .main {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 800px;
+    height: 800px;
+    margin-top: -400px;
+    margin-left: -400px;
+    background-color: #fff;
+    /* transition: width 3s, height 3s, transform 3s; */
+    animation: information 3s linear 1s;
+    border: 1px solid #fff;
+    
+    border-radius: 10px;
+    color: #585369;
+  }
+  
+  @keyframes information {
+    0% {
+      width: 800px;
+      height: 800px;
+      margin-top: -400px;
+      margin-left: -400px;
+      transform: rotate(0);
+    }
+    
+    50% {
+      width: 300px;
+      height: 300px;
+      margin-top: -150px;
+      margin-left: -150px;
+      transform: rotate(180deg);
+    }
+    
+    100% {
+      width: 800px;
+      height: 800px;
+      margin-top: -400px;
+      margin-left: -400px;
+      transform: rotate(360deg);
+    }
   }
 </style>
